0c9321508e0a37a2cc48d5be1a8ba40594c10e40
XiaoMengXinX <xibaole2333@gmail.com>
2022-09-10 03:04:43 +0800

    fix

--- api/webhook.go
+++ api/webhook.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 
@@ -49,6 +50,9 @@ func BotHandler(w http.ResponseWriter, r *http.Request) {
 		msg := tgbotapi.NewMessage(update.ChatJoinRequest.Chat.ID, fmt.Sprintf("你正在申请加入群组「%s」，请点击下方按钮以完成加群验证。", update.ChatJoinRequest.Chat.Title))
 		button := tgbotapi.NewInlineKeyboardButtonURL("开始验证", fmt.Sprintf("https://%s/captcha?token=%s", r.URL.Hostname(), tokenString))
 		msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(tgbotapi.NewInlineKeyboardRow(button))
-		_, _ = bot.Send(msg)
+		_, err = bot.Send(msg)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
